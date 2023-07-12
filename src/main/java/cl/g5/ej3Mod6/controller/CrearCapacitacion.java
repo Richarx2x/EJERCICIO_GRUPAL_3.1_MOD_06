500481cb672025d2fa22a36c96bdb41a7df3936a
Aracily <aaracily@gmail.com>
2023-07-11 23:18:35 -0400

    actualizo caso de uso ejercicio 3.1 falta css y bootstrap

--- src/main/java/cl/g5/ej3Mod6/controller/CrearCapacitacion.java
+++ src/main/java/cl/g5/ej3Mod6/controller/CrearCapacitacion.java
@@ -1,9 +1,13 @@
 package cl.g5.ej3Mod6.controller;
 
+import cl.g5.ej3Mod6.model.dto.CapacitacionDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/capacitacion")
@@ -14,4 +18,15 @@ public class CrearCapacitacion {
         return "crearCapacitacion";
     }
 
+    @GetMapping("/lista")
+    public String listaCapacitaciones(Model model){
+        List<CapacitacionDTO> caps = new ArrayList<>();
+        caps.add(new CapacitacionDTO("capacitacion1", "lista capacitacion uno"));
+        caps.add(new CapacitacionDTO("capacitacion2", "lista capacitacion dos"));
+        caps.add(new CapacitacionDTO("capacitacion3", "lista capacitacion tres"));
+        caps.add(new CapacitacionDTO("capacitacion4", "lista capacitacion cuatro"));
+        model.addAttribute("listaCaps",caps);
+        return "capacitaciones";
+    }
 }
+
